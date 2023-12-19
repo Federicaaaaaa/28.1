06a1095ea791eff07047907a1f79292ce501f660
Federicaaaaaa <danger2006.bona@gmail.com>
2023-12-19 16:47:09 +0100

    Aggiungere i file di progetto.

---
+++ 28.1/Scuola.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._1
+{
+    internal class Scuola
+    {
+        public List<Studente> Studenti { get; set; }
+        public List<Insegnante> Insegnanti { get; set; }
+    }
+}
---
+++ 28.1/Voto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._1
+{
+    internal class Voto
+    {
+        public DateTime Data { get; set; }
+        public Materia MateriaVoto { get; set; }
+        public int Valore { get; set; }
+
+    }
+}
